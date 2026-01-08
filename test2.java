d678ba1ce2140b3f854678b20b815b5ac6384d02
kamaleshwaran1745 <warankamalesh587@gmail.com>
2026-01-08 12:28:14 +0530

    Create test2.java

---
+++ test2.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+class test2{
+    public String toString()
+    {
+        return "Hello World";
+    }
+    public static void main(String args[]){
+        test2 t = new test2();
+        System.out.println(t);
+    }
+}
