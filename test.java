c774f3b6c3ab180d1b78a5c98360f1c8c445ae50
kamaleshwaran1745 <warankamalesh587@gmail.com>
2026-01-08 12:22:44 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public String toString(){
+        return "From method";
+    }
+    public static void main (String args[]){
+        test t = new test();
+        System.out.println(t);
+    }
+}
