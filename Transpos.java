9d5cdb27c70a0dd1baf7b5da6645bb5b6b2563ab
kamaleshwaran1745 <warankamalesh587@gmail.com>
2026-01-08 10:09:27 +0530

    Update Transpos.java

--- Transpos.java
+++ Transpos.java
@@ -1,7 +1,7 @@
 public class Transpos {
     public int[][] transpose(int[][] matrix) {
        int row = matrix.length;
-       int col = matrix[0].length;
+       int col = matrix[0].length; //Transpose matrix - 867
        int tran[][] = new int[col][row];
        for(int i=0;i<col;i++){
         for(int j=0;j<row;j++){
